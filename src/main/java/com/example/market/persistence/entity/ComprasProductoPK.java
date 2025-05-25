5230a26c16710ed1629fb14c699d1187e0c662d7
Julio Vazquez <juliovazquezm01@gmail.com>
2025-05-24 19:09:17 -0600

    contruyendo el API
    la notacion lombok no funciona, use map MapStruct, mejore las clases de dominio, decoradores para los metodos para el api, si causa error es por los tipos de datos
    - mapeadores
    - reposiotorios de producto orientado al dominio
    - implementacion de servicos de dominio
    - creacion del controlador REST
    - decoradores para las peticiones del api
    - anotaciones spring para exponer el api REST

--- src/main/java/com/example/market/persistence/entity/ComprasProductoPK.java
+++ src/main/java/com/example/market/persistence/entity/ComprasProductoPK.java
@@ -22,4 +22,21 @@ public class ComprasProductoPK implements Serializable {
 
     @Column(name = "id_producto")
     private Integer idProducto;
+
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
 }
